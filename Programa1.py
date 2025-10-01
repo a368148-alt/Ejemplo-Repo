9ca1454a485d8f99387bced620e1c37eceb04fea
a368148-alt <a368148@uach.mx>
2025-10-01 08:17:39 -0700

    Create Programa1.py

---
+++ Programa1.py
@@ -0,0 +1,6 @@
+#Este es un script de python de prueba para el repositorio
+def saludo(nombre):
+  return "Hola" + nombre
+
+#Se indica a la función saludo para saludar a alguien llamado Sebastian
+saludo("Sebastian")
